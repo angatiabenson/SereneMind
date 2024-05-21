bc067dc7211457d33759c22465374724226b1629
Angatia Benson <bensonetia@gmail.com>
2024-05-21 10:25:36 -0500

    Complete UI set up

--- app/src/main/java/ke/co/banit/serenemind/MainActivity.kt
+++ app/src/main/java/ke/co/banit/serenemind/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ke.co.banit.serenemind.ui.HomeScreen
 import ke.co.banit.serenemind.ui.theme.SereneMindTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SereneMindTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold {innerPadding ->
+                    HomeScreen(Modifier
+                        .padding(innerPadding))
                 }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SereneMindTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/ke/co/banit/serenemind/data/BottomMenuItem.kt
@@ -0,0 +1,8 @@
+package ke.co.banit.serenemind.data
+
+import androidx.annotation.DrawableRes
+
+data class BottomMenuItem(
+    val title: String,
+    @DrawableRes val icon: Int,
+)
